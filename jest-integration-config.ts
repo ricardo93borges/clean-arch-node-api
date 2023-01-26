fe92c582d9f35df491731ec109446ac265e2c37c
Ricardo Borges <ricardo93borges@gmail.com>
2023-01-26 20:53:30 +0000

    chore: add jest scripts

---
+++ jest-integration-config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import jestConfig from "./jest.config";
+
+// @ts-ignore
+jestConfig.testMatch = ["**/*.test.ts"];
+
+export default jestConfig;
---
+++ jest-unit-config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import jestConfig from "./jest.config";
+
+// @ts-ignore
+jestConfig.testMatch = ["**/*.spec.ts"];
+
+export default jestConfig;
