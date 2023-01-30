9344184914232f8228a2f1949c474c8be9bddae3
Ricardo Borges <ricardo93borges@gmail.com>
2023-01-30 21:39:53 +0000

    feat: add BcryptAdapter

---
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,37 @@
+import bcrypt from "bcrypt";
+import { BcryptAdapter } from "./bcrypt-adapter";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
+const salt = 12;
+const makeSut = (): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt);
+  return sut;
+};
+
+describe("Bcrypt Adapter", () => {
+  it("should call bcrypt with correct values", async () => {
+    const sut = makeSut();
+    const hashSpy = jest.spyOn(bcrypt, "hash");
+    await sut.encrypt("value");
+    expect(hashSpy).toHaveBeenCalledWith("value", salt);
+  });
+
+  it("should return a hash on success", async () => {
+    const sut = makeSut();
+    const hash = await sut.encrypt("value");
+    expect(hash).toBe("hash");
+  });
+
+  it("should throw if bcrypt throws", async () => {
+    const sut = makeSut();
+    //@ts-ignore
+    jest.spyOn(bcrypt, "hash").mockRejectedValue(new Error("some error"));
+    const promise = sut.encrypt("value");
+    await expect(promise).rejects.toThrow();
+  });
+});
