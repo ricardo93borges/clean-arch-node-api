77d3082d2487fc1b35f3391baf59f1aa9974274c
Ricardo Borges <ricardo93borges@gmail.com>
2023-01-30 22:44:47 +0000

    feat: add AccountMongoRepository.add

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,37 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
+  });
+
+  const makeSut = (): AccountMongoRepository => {
+    const sut = new AccountMongoRepository();
+    return sut;
+  };
+
+  it("should return account on success", async () => {
+    const sut = makeSut();
+    const account = await sut.add({
+      name: "name",
+      email: "email",
+      password: "password",
+    });
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toEqual("name");
+    expect(account.email).toEqual("email");
+    expect(account.password).toEqual("password");
+  });
+});
