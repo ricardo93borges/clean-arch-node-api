8eb6042bac9aa19d56bf26a63f18c40f2cb59f60
Ricardo Borges <ricardo93borges@gmail.com>
2023-01-31 21:48:05 +0000

    feat: add sign up route

---
+++ src/main/factory/signup.ts
@@ -0,0 +1,16 @@
+import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
+import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
+import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12;
+  const encrypter = new BcryptAdapter(salt);
+  const addAccountRepository = new AccountMongoRepository();
+  const dbAddAccount = new DbAddAccount(encrypter, addAccountRepository);
+  const emailValidator = new EmailValidatorAdapter();
+  const signUpController = new SignUpController(emailValidator, dbAddAccount);
+
+  return signUpController;
+};
