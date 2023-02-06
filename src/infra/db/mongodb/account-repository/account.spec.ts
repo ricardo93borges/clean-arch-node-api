1dd76b3e5e6d80c39fbc1cdcb0b25aac09980b97
Ricardo Borges <ricardo93borges@gmail.com>
2023-02-06 21:39:04 +0000

    feat: add loadByEmail and updateAccessToken methods

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,6 +1,8 @@
+import { Collection } from "mongodb";
 import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountMongoRepository } from "./account";
 
+let accountCollection: Collection;
 describe("Account Mongo Repository", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
@@ -11,7 +13,7 @@ describe("Account Mongo Repository", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -33,5 +35,27 @@ describe("Account Mongo Repository", () => {
     expect(account.name).toEqual("name");
     expect(account.email).toEqual("email");
     expect(account.password).toEqual("password");
+  });
+
+  it("should return an account on loadByEmail success", async () => {
+    const sut = makeSut();
+    await accountCollection.insertOne({
+      name: "name",
+      email: "email@email.com",
+      password: "password",
+    });
+    const account = await sut.loadByEmail("email@email.com");
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toEqual("name");
+    expect(account.email).toEqual("email@email.com");
+    expect(account.password).toEqual("password");
+  });
+
+  it("should return null if loadByEmail fails", async () => {
+    const sut = makeSut();
+    const account = await sut.loadByEmail("email@email.com");
+    expect(account).toBeFalsy();
   });
 });
