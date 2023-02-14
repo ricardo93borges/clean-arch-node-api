0fc27de1b152c68eb98b8f3ff7a2da374e7674fd
Ricardo Borges <ricardo93borges@gmail.com>
2023-02-14 21:38:23 +0000

    feat: add AddSurvey controller

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -0,0 +1,108 @@
+import {
+  AddSurvey,
+  AddSurveyModel,
+  HttpRequest,
+  Validation,
+} from "./add-survey-controller-protocols";
+import { AddSurveyController } from "./add-survey-controller";
+import {
+  badRequest,
+  noContent,
+  serverError,
+} from "../../../helpers/http/http-helper";
+
+interface SutTypes {
+  sut: AddSurveyController;
+  validationStub: Validation;
+  addSurveyStub: AddSurvey;
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    question: "question",
+    answers: [
+      {
+        image: "image",
+        answer: "answer",
+      },
+    ],
+  },
+});
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null;
+    }
+  }
+  const validationStub = new ValidationStub();
+  return validationStub;
+};
+
+const makeAddSurvey = (): AddSurvey => {
+  class AddSurveyStub implements AddSurvey {
+    add(data: AddSurveyModel): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+  const addSurveyStub = new AddSurveyStub();
+  return addSurveyStub;
+};
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const addSurveyStub = makeAddSurvey();
+  const sut = new AddSurveyController(validationStub, addSurveyStub);
+  return { sut, validationStub, addSurveyStub };
+};
+
+describe("AddSurvey Controller", () => {
+  it("should call validation with correct values", async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, "validate");
+    const httpRequest = makeFakeRequest();
+
+    await sut.handle(httpRequest);
+
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it("should 400 if validation fails", async () => {
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, "validate").mockReturnValueOnce(new Error());
+    const httpRequest = makeFakeRequest();
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(badRequest(new Error()));
+  });
+
+  it("should call AddSurvey with correct values", async () => {
+    const { sut, addSurveyStub } = makeSut();
+    const addSpy = jest.spyOn(addSurveyStub, "add");
+    const httpRequest = makeFakeRequest();
+
+    await sut.handle(httpRequest);
+
+    expect(addSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it("should return 500 if AddSurvey throws", async () => {
+    const { sut, addSurveyStub } = makeSut();
+    jest.spyOn(addSurveyStub, "add").mockRejectedValueOnce(new Error());
+    const httpRequest = makeFakeRequest();
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
+
+  it("should return 204 on success", async () => {
+    const { sut } = makeSut();
+    const httpRequest = makeFakeRequest();
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(noContent());
+  });
+});
---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,36 @@
+import {
+  badRequest,
+  noContent,
+  serverError,
+} from "../../../helpers/http/http-helper";
+import {
+  AddSurvey,
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation,
+} from "./add-survey-controller-protocols";
+
+export class AddSurveyController implements Controller {
+  constructor(
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { question, answers } = httpRequest.body;
+      const error = this.validation.validate(httpRequest.body);
+
+      if (error) {
+        return badRequest(error);
+      }
+
+      await this.addSurvey.add({ question, answers });
+
+      return noContent();
+    } catch (err) {
+      return serverError(err);
+    }
+  }
+}
