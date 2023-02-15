39b86e7dcbea49f1b5021230448a3d1633376453
Ricardo Borges <ricardo93borges@gmail.com>
2023-02-15 17:54:54 +0000

    feat: add SurveyMongoRepository

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,36 @@
+import { Collection } from "mongodb";
+import { MongoHelper } from "../helpers/mongo-helper";
+import { SurveyMongoRepository } from "./survey-mongo-repository";
+
+let collection: Collection;
+describe("Survey Mongo Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    collection = await MongoHelper.getCollection("surveys");
+    await collection.deleteMany({});
+  });
+
+  const makeSut = (): SurveyMongoRepository => {
+    const sut = new SurveyMongoRepository();
+    return sut;
+  };
+
+  it("should add a survey on success", async () => {
+    const sut = makeSut();
+
+    await sut.add({
+      question: "question",
+      answers: [{ image: "image", answer: "answer" }],
+    });
+    const survey = await collection.findOne({ question: "question" });
+
+    expect(survey).toBeTruthy();
+  });
+});
