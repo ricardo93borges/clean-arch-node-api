d65e6967b435b12d6108448d019bad3674390c2c
Ricardo Borges <ricardo93borges@gmail.com>
2023-02-16 17:07:43 +0000

    refactor: organize main layer folder

---
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -0,0 +1,22 @@
+import { LogMongoRepository } from "../../../../../infra/db/mongodb/log/log-mongo-repository";
+import { SignUpController } from "../../../../../presentation/controllers/login/signup/signup-controller";
+import { Controller } from "../../../../../presentation/protocols";
+import { LogControllerDecorator } from "../../../../decorators/log-controller-decorator";
+import { makeDbAddAccount } from "../../../usecases/account/add-account/db-add-account";
+import { makeDbAuthentication } from "../../../usecases/account/authentication/db-authentication-factory";
+
+import { makeSignUpValidation } from "./signup-validation-factory";
+
+export const makeSignUpController = (): Controller => {
+  const dbAddAccount = makeDbAddAccount();
+  const dbAuthentication = makeDbAuthentication();
+  const validationComposite = makeSignUpValidation();
+  const signUpController = new SignUpController(
+    dbAddAccount,
+    validationComposite,
+    dbAuthentication
+  );
+  const logMongoRepository = new LogMongoRepository();
+
+  return new LogControllerDecorator(signUpController, logMongoRepository);
+};
--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -1,11 +1,11 @@
 import {
   RequiredFieldValidation,
   ValidationComposite,
-} from "../../../../validation/validators";
-import { Validation } from "../../../../presentation/protocols/validation";
+} from "../../../../../validation/validators";
+import { Validation } from "../../../../../presentation/protocols/validation";
 import { makeAddSurveyValidation } from "./add-survey-validation-factory";
 
-jest.mock("../../../../validation/validators/validation-composite");
+jest.mock("../../../../../validation/validators/validation-composite");
 
 describe("AddSurveyValidation Factory", () => {
   it("should call ValidationComposite with all validations", () => {
