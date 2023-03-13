baa655733e78b1915b31bc2bfa2bb38f955df14d
Ricardo Borges <ricardo93borges@gmail.com>
2023-03-13 21:05:41 +0000

    feat: handle errors

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -2,11 +2,39 @@ import { Express } from "express";
 import { ApolloServer } from "apollo-server-express";
 import typeDefs from "@/main/graphql/type-defs";
 import resolvers from "@/main/graphql/resolvers";
+import { GraphQLError } from "graphql";
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach((error) => {
+    response.data = undefined;
+    if (checkError(error, "AuthenticationError")) {
+      response.http.status = 401;
+    } else if (checkError(error, "UserInputError")) {
+      response.http.status = 400;
+    } else if (checkError(error, "ForbiddenError")) {
+      response.http.status = 403;
+    } else {
+      response.http.status = 500;
+    }
+  });
+};
+
+const checkError = (error: GraphQLError, errorName: string) => {
+  return error.name === errorName || error.originalError.name === errorName;
+};
 
 export default async (app: Express): Promise<void> => {
   const server = new ApolloServer({
     resolvers,
     typeDefs,
+    plugins: [
+      {
+        requestDidStart: async () => ({
+          willSendResponse: async ({ response, errors }) =>
+            handleErrors(response, errors),
+        }),
+      },
+    ],
   });
 
   await server.start();
